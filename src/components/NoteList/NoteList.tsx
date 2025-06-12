55819b8fd81bc517051e1387449bc134cd206eb8
Nikita2993-Z <zinchenko.nikita93@gmail.com>
2025-06-12 21:35:06 +0300

    hw5

---
+++ src/components/NoteList/NoteList.tsx
@@ -0,0 +1,71 @@
+import cssStyles from "./NoteList.module.css";
+import type { Note } from "../../types/note";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteNote } from "../../services/noteService";
+import { PropagateLoader } from "react-spinners";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import { useState } from "react";
+
+interface NoteListProps {
+  notes: Note[];
+}
+
+export default function NoteList({ notes }: NoteListProps) {
+  const [deletingNoteId, setDeletingNoteId] = useState<Note["id"] | null>(null);
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: async (id: Note["id"]) => deleteNote(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["notes"] });
+      setDeletingNoteId(null);
+    },
+    onError: () => {
+      setDeletingNoteId(null);
+    },
+  });
+
+  const { isError } = mutation;
+
+  const handleDelete = (id: number) => {
+    setDeletingNoteId(id);
+    mutation.mutate(id);
+  };
+
+  return (
+    <>
+      <ul className={cssStyles.list}>
+        {notes.map((note) => {
+          return (
+            <li className={cssStyles.listItem} key={note.id}>
+              <h2 className={cssStyles.title}>{note.title}</h2>
+              <p className={cssStyles.content}>{note.content}</p>
+              <div className={cssStyles.footer}>
+                <span className={cssStyles.tag}>{note.tag}</span>
+                <button
+                  className={cssStyles.button}
+                  onClick={() => handleDelete(note.id)}
+                  disabled={deletingNoteId === note.id}
+                >
+                  {deletingNoteId !== note.id ? "Delete" : "In progress"}
+                  {deletingNoteId === note.id && (
+                    <PropagateLoader
+                      color="#0d6efd"
+                      cssOverride={{}}
+                      loading
+                      size={9}
+                      speedMultiplier={1}
+                    />
+                  )}
+                </button>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+
+      {isError && <ErrorMessage />}
+    </>
+  );
+}
---
+++ src/services/noteService.ts
@@ -0,0 +1,59 @@
+import axios from "axios";
+import type { Note } from "../types/note";
+
+const API_KEY = import.meta.env.VITE_NOTEHUB_TOKEN;
+
+interface NotesResponse {
+  notes: Note[];
+  totalPages: number;
+}
+
+export interface createNoteValues {
+  title: string;
+  content?: string;
+  tag: "Work" | "Personal" | "Meeting" | "Shopping" | "Todo";
+}
+
+interface SearchParams {
+  page: number;
+  perPage: number;
+  search?: string;
+}
+
+axios.defaults.baseURL = `https://notehub-public.goit.study/api`;
+axios.defaults.headers.common["Authorization"] = `Bearer ${API_KEY}`;
+
+export async function fetchNotes(
+  search: string,
+  page: number
+): Promise<NotesResponse> {
+  const perPage = 12;
+  const params: SearchParams = { page, perPage };
+
+  if (search) params.search = search;
+
+  const res = await axios.get<NotesResponse>("/notes", {
+    params,
+  });
+
+  return res.data;
+}
+
+export async function createNote({
+  title,
+  content,
+  tag,
+}: createNoteValues): Promise<Note> {
+  const res = await axios.post<Note>("/notes", {
+    title,
+    content,
+    tag,
+  });
+
+  return res.data;
+}
+
+export async function deleteNote(id: number): Promise<Note> {
+  const res = await axios.delete<Note>(`/notes/${id}`);
+  return res.data;
+}
